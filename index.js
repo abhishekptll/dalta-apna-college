d8f6c6cf1c30dfc4324834cad50fcfba9ba4c730
abhishekptll <abhishekptl9407@gmail.com>
2026-06-15 22:09:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// let obj = {
+//     name:"abhishek",
+//     marks:{
+//         math :70,
+//         hindi:50,
+//     }
+// }
+// obj.city="bhopal"
+// obj["contry"]="india"
+// obj.marks.eng = 90
+// console.log(obj.marks);
+
+
+// let student =[
+//     {
+//         name:"abhishek",
+//         age:20
+//     },
+//      {
+//         name:"abhishek",
+//         age:20
+//     },
+//      {
+//         name:"abhishek",
+//         age:20
+//     }
+// ]
+// console.log(student[0].name);
+
+// let num = Math.random()*100+1;
+// let number=Math.floor(num)
+// console.log(number);
